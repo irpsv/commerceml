17af82f165f15a82ab907ae04aae8fa6812e99e7
irpsv <i.rpsv@live.com>
2017-08-24 14:44:10 +0500

    add createFromProduct

--- src/Offer.php
+++ src/Offer.php
@@ -37,4 +37,62 @@ class Offer extends Product
 	{
 		return $this->storeCounts;
 	}
+
+	public static function createFromProduct(Product $product)
+	{
+		$offer = new self();
+		$offer->setId(
+			$product->getId()
+		);
+		$offer->setName(
+			$product->getName()
+		);
+		$offer->setDesc(
+			$product->getDesc()
+		);
+		$offer->setBarcode(
+			$product->getBarcode()
+		);
+		$offer->setArticul(
+			$product->getArticul()
+		);
+		$offer->setBaseScale(
+			$product->getBaseScale()
+		);
+		$offer->setContragentProductId(
+			$product->getContragentProductId()
+		);
+		$offer->setVendor(
+			$product->getVendor()
+		);
+		foreach ($product->getGroupsIds() as $item) {
+			$offer->addGroupId($item);
+		}
+		foreach ($product->getPictures() as $item) {
+			$offer->addPicture($item);
+		}
+		foreach ($product->getPropertyValues() as $item) {
+			$offer->addPropertyValue($item);
+		}
+		foreach ($product->getTaxRates() as $item) {
+			$offer->addTaxRate($item);
+		}
+		foreach ($product->getExcises() as $item) {
+			$offer->addExcise($item);
+		}
+		foreach ($product->getAccessories() as $item) {
+			$offer->addAccessory($item);
+		}
+		foreach ($product->getAnalogs() as $item) {
+			$offer->addAnalog($item);
+		}
+		foreach ($product->getProductChars() as $item) {
+			$offer->addProductChar($item);
+		}
+		foreach ($product->getRequisiteValues() as $item) {
+			$offer->addRequisiteValue($item);
+		}
+
+		return $offer;
+	}
 }
