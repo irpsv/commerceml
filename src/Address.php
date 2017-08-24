b2b241df26f11312448dd353f684f3e61d4f8dc7
irpsv <i.rpsv@live.com>
2017-08-24 09:59:43 +0500

    add Address

---
+++ src/Address.php
@@ -0,0 +1,38 @@
+<?php
+
+class Address
+{
+	protected $performance; // представление
+	protected $comment;
+	protected $addressFields = [];
+
+	public function setPerformance(string $value)
+	{
+		$this->performance = $value;
+	}
+
+	public function getPerformance(): ?string
+	{
+		return $this->performance;
+	}
+
+	public function setComment(string $value)
+	{
+		$this->comment = $value;
+	}
+
+	public function getComment(): ?string
+	{
+		return $this->comment;
+	}
+
+	public function addAddressField(AddressField $value)
+	{
+		$this->addressFields[] = $value;
+	}
+
+	public function getAddressFields(): array
+	{
+		return $this->addressFields;
+	}
+}
